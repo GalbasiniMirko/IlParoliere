10b22bdbab6836161466d0dd8c4982ba063d1c96
gaiac <gaia.casalini@edu.cobianchi.it>
2023-01-17 12:06:34 +0100

    EXAMPLE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao!");
+        System.out.println("GALBA SMETTILA DI LECCARE IL CULO E FEDE NON AGITARTI");
     }
 }
