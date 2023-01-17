310ed187d134295f56f0bf9282a8bcf7af004535
mirko <mirko.galbasini@edu.cobianchi.it>
2023-01-17 12:09:24 +0100

    ciao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("non vedo l'ora di iniziare questo progetto!");
+        System.out.println("nueqovbriyyabiakulfv");
     }
 }
