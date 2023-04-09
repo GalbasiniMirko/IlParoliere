41590c8095abcc56d8b18d73ae68e6e731ca2545
mirko <mirko.galbasini@edu.cobianchi.it>
2023-04-09 15:39:54 +0200

    Completamento primo frame, creato un JPanel che contiene un JLabel e tre JButton per fare in modo di poterlo cambiare con quello che conterrà le impostazioni di gioco

--- src/View/PrimaPagina.java
+++ src/View/PrimaPagina.java
@@ -2,15 +2,17 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PrimaPagina extends JFrame {
-
+public class PrimaPagina extends JFrame implements ActionListener {
     ImageIcon iconFrame = new ImageIcon("LogoProgettoGPO.png");
     JPanel panelPrimaPagina;
     JLabel labelTitolo;
     JButton btnGioca;
     JButton btnClassifica;
-    //eventualmente fare un bottone per uscire sotto questi due
+
+    JButton btnEsci;
 
     public PrimaPagina(){
         this.setTitle("Il Paroliere");
@@ -20,35 +22,63 @@ public class PrimaPagina extends JFrame {
         this.setBounds(250, 50, 1000, 700);
 
         labelTitolo = new JLabel();
+        labelTitolo.setBounds(0, 0, 1000, 250);
         labelTitolo.setText("IL PAROLIERE");
+        labelTitolo.setBackground(new Color(125, 125, 125));
+        labelTitolo.setOpaque(true);
         labelTitolo.setForeground(new Color(0, 0, 0));   //imposta colore del testo
-        labelTitolo.setFont(new Font("Comic Sans", Font.BOLD, 50));   //imposta il font e la grandezza del testo
-        labelTitolo.setVerticalAlignment(JLabel.TOP);   //imposta la posizione verticale del testo
+        labelTitolo.setFont(new Font("Comic Sans", Font.BOLD, 75));   //imposta il font e la grandezza del testo
+        labelTitolo.setVerticalAlignment(JLabel.CENTER);   //imposta la posizione verticale del testo
         labelTitolo.setHorizontalAlignment(JLabel.CENTER);   //imposta la posizione orizzontale del testo
 
         btnGioca = new JButton();
-        btnGioca.setBounds(200, 200, 100, 100);
+        btnGioca.setBounds(150, 325, 250, 100);
+        //btnGioca.addActionListener();
         btnGioca.setText("GIOCA!");
         btnGioca.setFocusable(false);
         btnGioca.setVerticalTextPosition(JButton.CENTER);
         btnGioca.setHorizontalTextPosition(JButton.CENTER);
+        btnGioca.setFont(new Font("Comic Sans", Font.BOLD, 30));
+        btnGioca.setForeground(Color.black);
+        btnGioca.setBackground(Color.lightGray);
 
         btnClassifica = new JButton();
-        btnClassifica.setBounds(400, 200, 100, 100);
+        btnClassifica.setBounds(600, 325, 250, 100);
+        //btnClassifica.addActionListener();
         btnClassifica.setText("CLASSIFICA");
-        btnGioca.setFocusable(false);
+        btnClassifica.setFocusable(false);
         btnClassifica.setVerticalTextPosition(JButton.CENTER);
         btnClassifica.setHorizontalTextPosition(JButton.CENTER);
+        btnClassifica.setFont(new Font("Comic Sans", Font.BOLD, 30));
+        btnClassifica.setForeground(Color.black);
+        btnClassifica.setBackground(Color.lightGray);
+
+        btnEsci = new JButton();
+        btnEsci.setBounds(425, 550, 150, 50);
+        //btnEsci.addActionListener();
+        btnEsci.setText("ESCI");
+        btnEsci.setFocusable(false);
+        btnEsci.setVerticalTextPosition(JButton.CENTER);
+        btnEsci.setHorizontalTextPosition(JButton.CENTER);
+        btnEsci.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        btnEsci.setForeground(Color.black);
+        btnEsci.setBackground(Color.lightGray);
 
         panelPrimaPagina = new JPanel();
         panelPrimaPagina.setBackground(new Color(216, 112, 124));
         panelPrimaPagina.setBounds(0, 0, 1000, 700);
-        panelPrimaPagina.setVisible(true);
+        panelPrimaPagina.setLayout(null);
+        panelPrimaPagina.add(labelTitolo);
         panelPrimaPagina.add(btnGioca);
         panelPrimaPagina.add(btnClassifica);
-        panelPrimaPagina.add(labelTitolo);
+        panelPrimaPagina.add(btnEsci);
 
         this.add(panelPrimaPagina);
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
