21a37f58384e54111ad4071050bbe8ebf6a316a6
gaiac <gaia.casalini@edu.cobianchi.it>
2023-04-17 10:45:23 +0200

    panel impostazioni gioco (senza action listener)

--- src/View/panelimpostazioni.java
+++ src/View/panelimpostazioni.java
@@ -21,7 +21,7 @@ public class panelimpostazioni extends JPanel implements ActionListener {
         this.setLayout(null);
 
         titolo= new JLabel();
-        titolo.setBounds(0, 0, 1000, 80);
+        titolo.setBounds(0, 0, 1000,  100);
         titolo.setText("IMPOSTAZIONI DI GIOCO");
         titolo.setBackground(new Color(125, 125, 125));
         titolo.setOpaque(true);
@@ -31,40 +31,49 @@ public class panelimpostazioni extends JPanel implements ActionListener {
         titolo.setHorizontalAlignment(JLabel.CENTER);
 
         modalita = new JLabel();
-        modalita.setBounds(0, 80, 1000, 50);
-        modalita.setText("come ti chiami amo?");
+        modalita.setBounds(0, 100, 1000, 70);
+        modalita.setText("come ti chiami ?");
         modalita.setForeground(new Color(0, 0, 0));
         modalita.setFont(new Font("Comic Sans", Font.BOLD, 25));
         modalita.setVerticalAlignment(JLabel.CENTER);
         modalita.setHorizontalAlignment(JLabel.CENTER);
 
         username = new JTextField();
-        username.setBounds(400,150,200 ,30);
+        username.setBounds(400,190,200 ,30);
         username.setPreferredSize(new Dimension(250,40));
         username.setFont(new Font("Comic Sans", Font.BOLD, 25));
         username.setForeground(new Color(0, 0, 0));
         username.setBackground(new Color(255 ,255,255));
 
         sceltadifficolta = new JLabel();
-        sceltadifficolta.setBounds(0, 200, 1000, 50);
-        sceltadifficolta.setText("a che difficoltà giochiamo teso?");
+        sceltadifficolta.setBounds(0, 230, 1000, 70);
+        sceltadifficolta.setText("a che difficoltà giochiamo?");
         sceltadifficolta.setForeground(new Color(0, 0, 0));
         sceltadifficolta.setFont(new Font("Comic Sans", Font.BOLD, 25));
         sceltadifficolta.setVerticalAlignment(JLabel.CENTER);
         sceltadifficolta.setHorizontalAlignment(JLabel.CENTER);
 
         facile = new JRadioButton("facile");
-        facile.setBounds(455,250,150,80);
+        facile.setBounds(455,280,150,80);
         facile.setBackground(new Color(216, 112, 124));
+        facile.setFont(new Font("Comic Sans", Font.BOLD, 20));
         medio =new JRadioButton("medio");
-        medio.setBounds(455,300,150,80);
+        medio.setBounds(455,330,150,80);
         medio.setBackground(new Color(216, 112, 124));
+        medio.setFont(new Font("Comic Sans", Font.BOLD, 20));
         difficile = new JRadioButton("difficile");
-        difficile.setBounds(455,350,150,80);
+        difficile.setBounds(455,380,150,80);
         difficile.setBackground(new Color(216, 112, 124));
+        difficile.setFont(new Font("Comic Sans", Font.BOLD, 20));
+
+        ButtonGroup group=new ButtonGroup();
+        group.add(facile);
+        group.add(medio);
+        group.add(difficile);
+
 
         avanti =new JButton();
-        avanti.setBounds(420, 450, 150, 50);
+        avanti.setBounds(430, 480, 150, 50);
         avanti.setText("INIZIAMO");
         avanti.setFocusable(false);
         avanti.setVerticalTextPosition(JButton.CENTER);
@@ -73,10 +82,6 @@ public class panelimpostazioni extends JPanel implements ActionListener {
         avanti.setForeground(Color.black);
         avanti.setBackground(Color.lightGray);
 
-        ButtonGroup group=new ButtonGroup();
-        group.add(facile);
-        group.add(medio);
-        group.add(difficile);
 
 
 
