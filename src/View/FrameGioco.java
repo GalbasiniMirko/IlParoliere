5d8f98b48d848cffe984eb3dfc41bc254b2c21b6
gaiac <gaia.casalini@edu.cobianchi.it>
2023-05-03 11:18:33 +0200

    griglia con bottoni

--- src/View/FrameGioco.java
+++ src/View/FrameGioco.java
@@ -11,6 +11,12 @@ public class FrameGioco extends JFrame implements ActionListener {
     JPanel panelcentro;
     JPanel panelbasso;
     JLabel titolo;
+    JButton bottone1;
+    JButton bottone2;
+    JButton bottone3;
+    JButton bottone4;
+    JButton bottone5;
+    JButton bottone6;
     public FrameGioco(){
         this.setTitle("Il Paroliere");
         this.setIconImage(iconFrame.getImage());
@@ -30,8 +36,47 @@ public class FrameGioco extends JFrame implements ActionListener {
        JPanel panelcentro = new JPanel();
        panelcentro.setBackground(new Color(216, 112, 124));
        panelcentro.setPreferredSize(new Dimension(100, 100));
+       panelcentro.setLayout(new GridLayout(3,3,10,10));
 
-       JPanel panelbasso = new JPanel();
+       bottone1 = new JButton();
+       bottone1.setText("lettera 1");
+       bottone1.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone1.setSize(150,50);
+
+       bottone2 = new JButton();
+       bottone2.setText("lettera 2");
+       bottone2.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone2.setSize(150,50);
+
+       bottone3 = new JButton();
+       bottone3.setText("lettera 3");
+       bottone3.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone3.setSize(150,50);
+
+       bottone4 = new JButton();
+       bottone4.setText("lettera 4");
+       bottone4.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone4.setSize(150,50);
+
+       bottone5 = new JButton();
+       bottone5.setText("lettera 5");
+       bottone5.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone5.setSize(150,50);
+
+       bottone6 = new JButton();
+       bottone6.setText("lettera 6");
+       bottone6.setFont(new Font("Comic Sans", Font.BOLD, 15));
+       bottone6.setSize(150,50);
+
+       panelcentro.add(bottone1);
+       panelcentro.add(bottone2);
+       panelcentro.add(bottone3);
+       panelcentro.add(bottone4);
+       panelcentro.add(bottone5);
+       panelcentro.add(bottone6);
+
+
+        JPanel panelbasso = new JPanel();
        panelbasso.setBackground(new Color(253, 168, 90));
        panelbasso.setPreferredSize(new Dimension(100, 150));
 
